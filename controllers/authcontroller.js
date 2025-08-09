55e8c675b41f9b76a617d1ff5de9e944b4567efb
shashankBgowda <shashankbg74@gmail.com>
2025-08-09 11:49:06 -0500

    User created and stores in MongoDB

---
+++ controllers/authcontroller.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    // Validate input
+    if(!name || !email || !password) {
+        return res.json({ success:false, statuscode:400, statmessage: "Please fill all the fields" });
+    }
+    try {
+        // Check if user already exists
+        const existingUser = await userModel.findOne({email});
+
+        if(existingUser) {
+            return res.json({ success: false, statuscode: 400, statmessage: "User already exists", message: "User with this email already exists" });
+        }           
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const User = new userModel({name,email,password: hashedPassword});
+        await User.save(); // Save user to database
+        return res.json({ success: true, statuscode: 201, message: "User registered successfully" });
+    } catch (error) {
+        return res.json({ success: false, statuscode: 500, statmessage: "Internal server error" , message: error.message });
+    } 
+}
